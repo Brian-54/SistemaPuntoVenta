cba8890d4300ede33a1ec0f84fdfbeac365981dc
Brian-54 <71730396+Brian-54@users.noreply.github.com>
2020-12-10 10:40:44 -0600

    Se agrego metodo ingresar productos tabla

--- src/Vista/Sistema.java
+++ src/Vista/Sistema.java
@@ -9,6 +9,7 @@ import Modelo.Proveedor;
 import Modelo.ProveedorDao;
 import Reportes.Excel;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
@@ -23,6 +24,7 @@ public class Sistema extends javax.swing.JFrame {
     Productos pro=new Productos();
     ProductosDao proDao=new ProductosDao();
     DefaultTableModel modelo=new DefaultTableModel();
+    int item;
     /**
      * Creates new form Sistema
      */
@@ -309,6 +311,12 @@ public class Sistema extends javax.swing.JFrame {
         txtCodigoVenta.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyPressed(java.awt.event.KeyEvent evt) {
                 txtCodigoVentaKeyPressed(evt);
+            }
+        });
+
+        txtCantidadVenta.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                txtCantidadVentaKeyPressed(evt);
             }
         });
 
@@ -1261,6 +1269,42 @@ public class Sistema extends javax.swing.JFrame {
             }
         }
     }//GEN-LAST:event_txtCodigoVentaKeyPressed
+
+    private void txtCantidadVentaKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtCantidadVentaKeyPressed
+        if(evt.getKeyCode()==KeyEvent.VK_ENTER){
+             if(!"".equals(txtCantidadVenta.getText())){
+                 String cod=txtCodigoVenta.getText();
+                 String descripcion=txtDescripcionVenta.getText();
+                 int cant=Integer.parseInt(txtCantidadVenta.getText());
+                 double precio=Double.parseDouble(txtPrecioVenta.getText());
+                 double total= cant*precio;
+                 int stock=Integer.parseInt(txtStockDisponible.getText());
+                 if(stock >= cant){
+                     item=item+1;
+                     modelo=(DefaultTableModel) TableVenta.getModel();
+                     ArrayList lista= new ArrayList();
+                     lista.add(item);
+                     lista.add(cod);
+                     lista.add(descripcion);
+                     lista.add(cant);
+                     lista.add(precio);
+                     lista.add(total);
+                     Object[] O=new Object[5];
+                     O[0] = lista.get(1);
+                     O[1] = lista.get(2);
+                     O[2] = lista.get(3);
+                     O[3] = lista.get(4);
+                     O[4] = lista.get(5); 
+                     modelo.addRow(O);
+                     TableVenta.setModel(modelo);
+                 }else{
+                     JOptionPane.showMessageDialog(null,"Stock no disponible");
+                 }
+             }else{
+                  JOptionPane.showMessageDialog(null,"Ingrese Cantidad");
+             }
+        }
+    }//GEN-LAST:event_txtCantidadVentaKeyPressed
 
     /**
      * @param args the command line arguments
